e1e35ab596bb346053249c15a23ca2bf3c29c634
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2025-12-01 23:21:02 +0530

    Create max_of_three.py

---
+++ max_of_three.py
@@ -0,0 +1,9 @@
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second Number: "))
+c=int(input("Enter Third Number: "))
+if(a>b and a>c):
+   print(a, "is the max number")
+elif(b>a and b>c):
+  print(b, "is the max number")
+else:
+  print(c, "is the max number")
