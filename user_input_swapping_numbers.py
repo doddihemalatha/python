e0f14911ec3fa629448bcc1b5466686779fc7802
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-07-19 11:39:54 +0530

    Create user_input_swapping_numbers.py

---
+++ user_input_swapping_numbers.py
@@ -0,0 +1,13 @@
+a=int(input("Enter a value":))
+b=int(input("Enter b value:"))
+
+print("*"*8,"Before Swapping","*"*8)
+print("a=",a)
+print("b=",b)
+
+print("*"*8,"After Swapping","*"*8)
+temp=a
+a=b
+b=temp
+print("a=",a)
+print("b=",b)
