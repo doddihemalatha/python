b46b4a5df6de0482254b9fc6ffda5f80c8f5c213
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-07-20 20:07:39 +0530

    Create simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,21 @@
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+
+print("Addition is:",a+b)
+print("subtraction is:",a-b)
+print("multiplication is:",a*b)
+print("Division is:", a/b)
+
+# Or in python we can also write like this
+first_no=float(input("Enter first number:"))
+second_no=float(input("Enter second number:"))
+
+addition=first_no + second_no
+subtraction=first_no - second_no
+multiplication=first_no * second_no
+division=first_no / second_no
+
+print("Addition is:",addition)
+print("Subtreaction is:",subtraction)
+print("Multiplication is",multiplication)
+print("Division is", division)
