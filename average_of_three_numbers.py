0cca4c6fd191a4646c3d0a55dfbb4fe2804cbb58
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-07-20 23:35:12 +0530

    Add script to calculate average of three numbers

---
+++ average_of_three_numbers.py
@@ -0,0 +1,10 @@
+#average of 3 numbers
+#formula (x+y+z)/3
+
+first_no=float(input("Enter First Number:"))
+second_no=float(input("Enter Second Number:"))
+third_no=float(input("Enter Third Number:"))
+
+total=first_no + second_no + third_no
+average=total/3
+print("The Average of 3 Numbers:",average)
