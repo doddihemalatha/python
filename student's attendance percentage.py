feab4477bddea09382cced0bfae8dc00e9ee4f8a
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-07-21 23:17:43 +0530

    Create student's attendance percentage.py

---
+++ student's attendance percentage.py
@@ -0,0 +1,8 @@
+#student's attendance percentage
+
+student_attendance=float(input("Enter Student Attendance Percentage:"))
+
+if student_attendance>=75:
+    print("\nAllowed to Write Exam")
+else:
+    print("\nNot Allowed")
