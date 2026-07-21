a2f2f59602efb1ec49b71c57db1d783c78103a80
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-07-21 21:01:34 +0530

    Create equal_or_different.py

---
+++ equal_or_different.py
@@ -0,0 +1,9 @@
+#Equal or Different
+
+num1=float(input("Enter First Number:"))
+num2=float(input("Enter Second Number:"))
+
+if num1==num2:
+    print("Both numbers are equal")
+else:
+    print("Both numbers are different")
