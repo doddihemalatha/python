3c146a077ddc75406188139cda68565e2891c4c9
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-07-28 19:56:44 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,16 @@
+alphabet=input("enter a letter:").lower()
+
+if len(alphabet)!=1 or not alphabet.isalpha():
+    print("Invalid Input")
+elif alphabet=="a":
+    print("Vowel")
+elif alphabet=="e":
+    print("Vowel")
+elif alphabet=="i":
+    print("Vowel")
+elif alphabet=="o":
+    print("Vowel")
+elif alphabet=="u":
+    print("Vowel")
+else:
+    print("Consonent")
