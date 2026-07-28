701d60b3bf340705ae09d8d3b296751caa1d71b7
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-07-28 18:09:58 +0530

    Create largest_of_2_numbers.py

---
+++ largest_of_2_numbers.py
@@ -0,0 +1,8 @@
+num1=float(input("Enter first Number:")) 
+num2=float(input("Enter second number:"))
+if num1>num2:
+  print("First number",num1,"is larger")
+elif num2>num1:
+  print("Second number",num2,"is larger") 
+else:
+  print("Both Numbers are Equal")
