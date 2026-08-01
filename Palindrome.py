1dce19fc27f726bcb96de86259e383db08e9cfe6
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-01 19:11:28 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,15 +1,18 @@
-num = abs(int(input("Enter a number:")))
+num = int(input("Enter a number:"))
 
-original = num
-reverse = 0
-
-
-while num>0:
-    last_digit = num%10
-    reverse = reverse * 10 + last_digit
-    num = num // 10
-
-if original == reverse:
-    print("Palindrome")
+if num <0:
+    print("  not palindrome")
 else:
-    print("Not palindrome")
+    original = num
+    reverse = 0
+
+
+    while num>0:
+        last_digit = num%10
+        reverse = reverse * 10 + last_digit
+        num = num // 10
+
+    if original == reverse:
+        print("Palindrome")
+    else:
+        print("Not palindrome")
