f6763ed8771318ec78c42a892c34a6e7150ff9de
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-01 22:29:45 +0530

    Update Prime_numbers.py

--- Prime_numbers.py
+++ Prime_numbers.py
@@ -1,14 +1,17 @@
-num = abs(int(input("Enter a Number")))
+num = int(input("Enter Number: "))
 
-x = 1
-count = 0
-
-while x <= num:
-    if num % x == 0:
-        count+=1
-    x+=1
-  
-if count == 2:
-    print("Prime Number")
-else:
+if num <= 1:
     print("Not Prime Number")
+else:
+    x = 1
+    count = 0
+
+    while x <= num:
+        if num % x == 0:
+            count += 1
+        x += 1
+
+    if count == 2:
+        print("Prime Number")
+    else:
+        print("Not Prime Number")
