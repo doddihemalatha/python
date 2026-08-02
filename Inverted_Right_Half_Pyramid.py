26a0826922efa9e6f46a8c2a4eef64126947d4e7
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-02 23:05:26 +0530

    Update Inverted_Right_Half_Pyramid.py

--- Inverted_Right_Half_Pyramid.py
+++ Inverted_Right_Half_Pyramid.py
@@ -4,3 +4,10 @@ for row in range(7,0,-1):
     for star in range(1,row-1):
         print("*",end="")
     print()
+
+           #  OR
+
+for row in range(5,0,-1):
+    for star in range(1,row+1):
+        print("*",end="")
+    print()
