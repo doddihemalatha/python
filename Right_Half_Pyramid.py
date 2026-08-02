c87c4c312c340c57d9ad3eeb53276810da59e278
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-02 22:51:06 +0530

    Create Right_Half_Pyramid.py

---
+++ Right_Half_Pyramid.py
@@ -0,0 +1,8 @@
+
+
+for row in range(1,6):       #how many rows 
+
+    for star in range(1,row+1):              #How many stars in that row
+        print("*",end="")        #end="" -->it says don't go to the next line
+    print() #print() --> move to the next line
+  
